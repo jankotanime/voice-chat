cc4a4432b9048888b42f858170a8dcdc37937b67
jankotanime <jan.gasztold@gmail.com>
2025-05-05 19:40:40 +0200

    init: docker

--- voice-service/server.js
+++ voice-service/server.js
@@ -2,12 +2,12 @@ import express from 'express';
 import http from 'http';
 
 const server = express();
-const port = 8001;
+const port = 8002;
 
 server.use(express.json());
 
 server.get('/test', (req, res) => {
-  res.send('Hello from user service!');
+  res.send('Hello from voice service!');
 })
 
 http.createServer(server).listen(port, () => {
