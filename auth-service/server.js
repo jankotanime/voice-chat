414024fc614c721f4a881b19350477e6378b4551
jankotanime <jan.gasztold@gmail.com>
2025-05-06 21:54:36 +0200

    init: keycloak - auth services

--- auth-service/server.js
+++ auth-service/server.js
@@ -1,17 +1,71 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import jwksClient from 'jwks-rsa';
+import axios from 'axios';
 
 const server = express();
 const port = 8003;
+const keycloakUrl = process.env.KEYCLOAK_URL
+const secretKey = process.env.SECRET_KEY;
 
 server.use(express.json());
 
-server.get('/test', (req, res) => {
-  res.send('Hello from auth service!');
-})
+const client = jwksClient({
+  jwksUri: `${keycloakUrl}/realms/voice-chat/protocol/openid-connect/certs`
+});
 
-server.get('/keycloak/authorization', (req, res) => {
-  res.status(400).send('nie ok');
-})
+const getKey = (header, callback) => {
+  client.getSigningKey(header.kid, (err, key) => {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, key.getPublicKey());
+  });
+};
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(403).json({ message: 'No token provided' });
+  }
+
+  const tokenWithoutBearer = token.split(' ')[1];
+
+  jwt.verify(tokenWithoutBearer, getKey, { algorithms: ['RS256'] }, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Failed to authenticate token' });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+server.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const response = await axios.post(
+      `${keycloakUrl}/realms/voice-chat/protocol/openid-connect/token`,
+      new URLSearchParams({
+        client_id: 'SPA',
+        client_secret: 'iLbMetMmbMWNWdkU8F936vrNIEXMSD6j',
+        grant_type: 'password',
+        username: username,
+        password: password,
+      }),
+      { headers: { 'Content-Type': 'application/x-www-form-urlencoded' }}
+    );
+    const token = response.data.access_token;
+    console.log("logged:", username);
+    res.status(200).json({ token: token });
+  } catch (err) {
+    console.error('Login failed:', err);
+  }
+});
+
+server.get('/verify-test', verifyToken, async (req, res) => {
+  console.log(req.user)
+  res.status(200).json({ mess: "verifed" })
+});
 
 server.listen(port, () => {
   console.log(`Serwer działa na http://localhost:${port}`);
