3dd3855cce00a7b54aca32bdbf5d940dfd5d1c48
jankotanime <jan.gasztold@gmail.com>
2025-05-10 14:43:53 +0200

    socket init

--- voice-service/server.js
+++ voice-service/server.js
@@ -1,14 +1,36 @@
-import express from 'express';
+import connect from 'connect';
+import { Server } from 'socket.io'
 
-const server = express();
+const app = connect();
 const port = 8002;
 
-server.use(express.json());
+const io = new Server(app, {cors: {
+  origin: "https://localhost:8002",
+  methods: ["GET", "POST"]
+}})
 
-server.get('/test', (req, res) => {
-  res.send('Hello from voice service!');
+app.use(serveStatic("public"))
+
+
+io.sockets.on("connection", (socket) => {
+  socket.on("join_room", (user, room) => {
+      socket.userId = user;
+      socket.join(room)
+      console.log(`${user} dołączył do pokoju: global`)
+  })
+  socket.on("message", (data) => {
+      console.log(`${data['user']}: ${data['message']}`)
+      io.to(data['room']).emit("message_global", data['user'], data['message']);
+  })
 })
 
-server.listen(port, () => {
+io.sockets.on("connect_error", () => {
+  console.log("Błąd połączenia");
+  setTimeout(() => {
+      socket.connect();
+  }, 2000)
+})
+
+app.listen(port, () => {
   console.log(`Serwer działa na http://localhost:${port}`);
 });
