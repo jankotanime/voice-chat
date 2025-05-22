2c8e621fea9b8f46f743826637479dba33b56b92
jankotanime <jan.gasztold@gmail.com>
2025-05-22 22:27:55 +0200

    add: working ssr

---
+++ client/ssr/src/fetchData.js
@@ -0,0 +1,26 @@
+const fetchData = async (token) => {
+  if (token) {
+    try {
+      const response = await fetch(`http://192.168.0.12:8001/user`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      });
+
+      if (!response.ok) {
+        return {error: "Przepraszamy błąd po stronie serwera"}
+      }
+
+      const usersData = await response.json();
+      return usersData
+    } catch (error) {
+      return {error: "Błąd podcas pobierania danych"}
+    }
+  } else {
+      return {error: "Brak tokenu"}
+  }
+}
+
+export default fetchData
--- client/ssr/src/server.js
+++ client/ssr/src/server.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import session from 'express-session';
 import Keycloak from 'keycloak-connect';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fetchData from './fetchData.js';
 
 const app = express();
 const port = 3001;
 
 const memoryStore = new session.MemoryStore();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(session({
   secret: 'secret',
@@ -27,29 +33,34 @@ app.use(keycloak.middleware({
   logout: '/logout',
   admin: '/',
   protected: '/',
-  redirectUri: 'http://localhost:3001/'
+  redirectUri: 'http://192.168.0.12:3001/'
 }));
+
+app.set('views', path.join(__dirname, '..', 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', keycloak.protect(), async (req, res) => {
   const token = req.kauth.grant.access_token.token;
 
   if (token) {
     try {
-      const response = await fetch(`http://192.168.0.12:8001/user`, {
-        method: 'GET',
+      const response = await fetch(`http://192.168.0.12:8001/user/admin`, {
+        method: 'POST',
         headers: {
           'Authorization': `Bearer ${token}`,
           'Content-Type': 'application/json'
         }
       });
-
+      if (response.status === 403) {
+        return res.render('forbidden.ejs'); 
+      }
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
-      const json = await response.json();
-      res.send(json);
-
+      const json = await fetchData(token);
+      console.log(json)
+      res.render('index.ejs', { users: json }); 
     } catch (error) {
       console.error('Błąd podczas pobierania danych:', error);
       res.status(500).send('Błąd serwera podczas pobierania danych');
@@ -59,6 +70,16 @@ app.get('/', keycloak.protect(), async (req, res) => {
   }
 });
 
+app.get('/logout', (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) {
+      console.error('Błąd podczas niszczenia sesji:', err);
+      return next(err);
+    }
+    const logoutUrl = 'http://192.168.0.12:8080/auth/realms/voice-chat/protocol/openid-connect/logout?redirect_uri=http://192.168.0.12:3001/';
+    res.redirect(logoutUrl);
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
