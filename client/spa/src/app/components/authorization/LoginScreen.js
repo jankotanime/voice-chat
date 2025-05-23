d38527f149659436c53cd7f38470e7d3f1629439
jankotanime <jan.gasztold@gmail.com>
2025-05-23 14:50:24 +0200

    finish: ssr

--- client/spa/src/app/components/authorization/LoginScreen.js
+++ client/spa/src/app/components/authorization/LoginScreen.js
@@ -5,10 +5,15 @@ import { useKeycloak } from './../../auth/provider/KeycloakProvider.js';
 const LoginScreen = () => {
   const { login } = useKeycloak();
 
+  const toAdminPanel = () => {
+    window.location.href = 'http://localhost:3001';
+  }
+
   return (<div className="center">
           <div>Please login to continue</div>
           <div>
             <button className="loginButton" onClick={login}>Click to login or to register</button>
+            <button className="loginButton" onClick={toAdminPanel}>Click to go to admin panel</button>
           </div>
         </div>)
 }
