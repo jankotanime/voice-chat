60e341aaf73a0baef4ce583b2d570aa007738b02
jankotanime <jan.gasztold@gmail.com>
2025-06-01 23:29:27 +0200

    finish: kubernetes

---
+++ deploy_k8s.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+echo "=== Tworzenie PersistentVolume i PersistentVolumeClaim ==="
+kubectl apply -f k8s/mongo/mongo-dump-pv.yaml
+kubectl apply -f k8s/mongo/mongo-dump-pvc.yaml
+kubectl apply -f k8s/keycloak/keycloak-pv.yaml
+kubectl apply -f k8s/keycloak/keycloak-pvc.yaml
+kubectl apply -f k8s/postgres/pv.yaml
+kubectl apply -f k8s/postgres/pvc.yaml
+
+echo "=== Tworzenie Postgresa ==="
+kubectl apply -f k8s/postgres/service.yaml
+kubectl apply -f k8s/postgres/deployment.yaml
+
+echo "=== Tworzenie ConfigMapy z realmem ==="
+kubectl delete configmap keycloak-realm --ignore-not-found
+kubectl create configmap keycloak-realm --from-file=realm-export.json=realm-export.json
+
+echo "=== Tworzenie Service i Deployment dla MongoDB ==="
+kubectl apply -f k8s/mongo/mongo-service.yaml
+kubectl apply -f k8s/mongo/mongo-deployment.yaml
+
+echo "=== Tworzenie Service i Deployment dla Keycloak ==="
+kubectl apply -f k8s/keycloak/keycloak-service.yaml
+kubectl apply -f k8s/keycloak/keycloak-deployment.yaml
+
+echo "=== Tworzenie Service i Deployment dla User Service ==="
+kubectl apply -f k8s/user/user-service-deployment.yaml
+
+echo "=== Tworzenie Service i Deployment dla Voice Service ==="
+kubectl apply -f k8s/voice/voice-service-deployment.yaml
+
+echo "=== Tworzenie Service i Deployment dla SSR ==="
+kubectl apply -f k8s/ssr/ssr-deployment.yaml
+
+echo "=== Tworzenie Service i Deployment dla SPA ==="
+kubectl apply -f k8s/spa/spa-deployment.yaml
+
+echo "=== Tworzenie Ingress ==="
+kubectl apply -f k8s/ingress.yaml
+
+echo "=== Port-forwarding serwisów (CTRL+C aby przerwać) ==="
+
+kubectl port-forward service/spa 3000:3000 &
+kubectl port-forward service/ssr 3001:3001 &
+kubectl port-forward service/user-service 8001:8001 &
+kubectl port-forward service/voice-service 8002:8002 &
+kubectl port-forward service/keycloak 8080:8080 &
+kubectl port-forward service/mongo 27017:27017 &
+
+wait
