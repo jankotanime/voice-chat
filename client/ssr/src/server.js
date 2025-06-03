d19e20a9ae24a952e19824c28df41865467a363f
jankotanime <jan.gasztold@gmail.com>
2025-06-03 19:28:22 +0200

    finish: kubernetes; no ssr

--- client/ssr/src/server.js
+++ client/ssr/src/server.js
@@ -4,18 +4,28 @@ import Keycloak from 'keycloak-connect';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fetchData from './fetchData.js';
+import fs from "fs"
 
 const app = express();
 const port = 3001;
 
+function readSecret(path) {
+  return fs.existsSync(path) ? fs.readFileSync(path, 'utf8').trim() : null;
+}
+
 const IP = process.env.IP
-const KEYCLOAK_URL = process.env.KEYCLOAK_URL
+const KEYCLOAK_INT_URL = process.env.KEYCLOAK_INT_URL
+const KEYCLOAK_PUB_URL = process.env.KEYCLOAK_PUB_URL
 const USER_URL = process.env.USER_URL
+const SSR_URL = process.env.SSR_URL
+const secret = readSecret('/app/secrets/backend_secret');
 
 const memoryStore = new session.MemoryStore();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+app.set('trust proxy', 1);
 
 app.use(session({
   secret: 'secret',
@@ -27,29 +37,32 @@ app.use(session({
 const keycloakConfig = {
   clientId: 'SSR',
   bearerOnly: false,
-  serverUrl: KEYCLOAK_URL,
+  serverUrl: KEYCLOAK_INT_URL,
   realm: 'voice-chat',
+  secret: secret
 };
 
 const keycloak = new Keycloak({ store: memoryStore }, keycloakConfig);
 
 app.use(keycloak.middleware({
-  logout: '/logout',
-  admin: '/',
-  protected: '/',
-  redirectUri: `http://${IP}:${port}/`
+  logout: '/ssr/logout',
+  admin: '/ssr',
+  protected: '/ssr',
+  redirectUri: process.env.SSR_URL + "/ssr"
 }));
 
-app.set('views', path.join(__dirname, '..', 'views'));
+app.set('views', path.join(__dirname, '.', 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', keycloak.protect(), async (req, res) => {
+app.get('/ssr', keycloak.protect({ redirectTo: `${KEYCLOAK_PUB_URL}/ssr` }), async (req, res) => {
+  console.log("aaa")
   const token = req.kauth.grant.access_token.token;
-
+  console.log(token)
   if (token) {
     try {
+      console.log("przed fetch")
       const response = await fetch(`${USER_URL}/user/admin`, {
         method: 'POST',
         headers: {
@@ -57,13 +70,15 @@ app.get('/', keycloak.protect(), async (req, res) => {
           'Content-Type': 'application/json'
         }
       });
+      console.log(response)
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
-
+      console.log("aaa")
       if (response.ok) {
         const json = (await response.json()).mess;
         if (!json) {
+          console.log(json)
           res.render('forbidden.ejs'); 
           return;
         }
@@ -80,13 +95,13 @@ app.get('/', keycloak.protect(), async (req, res) => {
   }
 });
 
-app.get('/logout', (req, res, next) => {
+app.get('/ssr/logout', (req, res, next) => {
   req.session.destroy(err => {
     if (err) {
       console.error('Błąd podczas niszczenia sesji:', err);
       return next(err);
     }
-    const logoutUrl = `${KEYCLOAK_URL}/auth/realms/voice-chat/protocol/openid-connect/logout?redirect_uri=http://${IP}:${port}/`;
+    const logoutUrl = `${KEYCLOAK_PUB_URL}/auth/realms/voice-chat/protocol/openid-connect/logout?redirect_uri=${SSR_URL}`;
     res.redirect(logoutUrl);
   });
 });
